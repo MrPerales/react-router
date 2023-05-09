eb002fe5b09421fc64e803d6972782fbc9ee2c80
Carlos Perales <carlosperales050@gmail.com>
2023-05-08 20:46:37 -0600

    comienza el ruteo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
+import { HashRouter, Route, Routes } from 'react-router-dom'
 import './App.css';
+import { HomePage } from './HomePage';
+import { BlogPage } from './BlogPage';
+import { ProfilePage } from './ProfilePage';
+import { NotFoundPage } from './NotFoundPage';
+import { Menu } from './Menu';
+
+//  /#/ -> principal page
+//  /#/blog -> BlogPage
+// .....
+//  /#/hdsnjfdsj -> not Found page
+
+// la propiedad element puede cambiar dependiendo si estamos usando
+// HashRouter o BrowserRouter 
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <HashRouter>
+        <Menu/>
+
+        <Routes>
+            
+          <Route path='/home' element={<HomePage/>}/>
+          <Route path='/blog' element={<BlogPage/>}/>
+          <Route path='/profile' element={<ProfilePage/>}/>
+          <Route path='*' element={<NotFoundPage/>}/>
+
+
+
+        </Routes>
+      
+      </HashRouter>
+    </>
   );
 }
 
