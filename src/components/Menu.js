dc0eea9dbe361c11265e941c8f6fd822252dc11a
Carlos Perales <carlosperales050@gmail.com>
2023-05-11 19:11:04 -0600

    rutas publicas y privadas

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,34 +1,38 @@
 import React from "react";
 import { Link, NavLink } from "react-router-dom";
+import { useAuth } from "../auth";
 
 function Menu() {
+    // importamos para tener la propiedad user para saber si estan autentificdos 
+    const auth = useAuth();
+
     return (
         <nav>
             <ul>
-                {/* with link */}
-                {/* {routes.map((item) => (
-                    <li>
-                        <Link to={`/${item.to}`}>{item.text}</Link>
-                    </li>
-
-                ))} */}
 
                 {/*with NavLink  */}
-                {routes.map(item => (
-                            
-                    <li key={item.to}>
+                {routes.map(item => {
+                    // si private= true y no hay usuario no esta autentificado return null 
+                    if (item.private && !auth.user) return null;
+                    // para ocultar el enlace asi login
+                    if (item.text === 'login' && auth.user) return null;
 
-                        <NavLink
-                            style={({ isActive }) => ({
-                                color: isActive ? 'red' : 'blue',
-                            })}
-                            to={item.to}
-                        >
-                            {item.text}
-                        </NavLink>
+                    return (
+                        <li key={item.to}>
 
-                    </li>
-                ))}
+                            <NavLink
+                                style={({ isActive }) => ({
+                                    color: isActive ? 'red' : 'blue',
+                                })}
+                                to={item.to}
+                            >
+                                {item.text}
+                            </NavLink>
+
+                        </li>
+                    )
+
+                })}
 
             </ul>
         </nav>
@@ -39,23 +43,28 @@ function Menu() {
 const routes = [];
 routes.push({
     to: '/',
-    text: 'Home'
+    text: 'Home',
+    private: false
 })
 routes.push({
     to: '/blog',
-    text: 'Blog'
+    text: 'Blog',
+    private: false
 })
 routes.push({
     to: '/profile',
-    text: 'profile'
+    text: 'profile',
+    private: true
 })
 routes.push({
     to: '/login',
-    text: 'login'
+    text: 'login',
+    private: false
 })
 routes.push({
     to: '/logout',
-    text: 'logout'
+    text: 'logout',
+    private: true
 })
 
 export { Menu }
